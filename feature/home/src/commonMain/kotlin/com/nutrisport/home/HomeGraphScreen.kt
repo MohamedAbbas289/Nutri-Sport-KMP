effe4bade2841a3a310543eaf41845c3402c2644
MohamedAbbas <midooabbas289@gmail.com>
2025-05-27 11:53:13 +0300

    + Custom Drawer Component

--- feature/home/src/commonMain/kotlin/com/nutrisport/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/com/nutrisport/home/HomeGraphScreen.kt
@@ -1,12 +1,14 @@
 package com.nutrisport.home
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,12 +27,14 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.nutrisport.home.component.BottomBar
+import com.nutrisport.home.component.CustomDrawer
 import com.nutrisport.home.domain.BottomBarDestination
 import com.nutrisport.shared.BebasNeueFont
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.IconPrimary
 import com.nutrisport.shared.Resources
 import com.nutrisport.shared.Surface
+import com.nutrisport.shared.SurfaceLighter
 import com.nutrisport.shared.TextPrimary
 import com.nutrisport.shared.navigation.Screen
 import org.jetbrains.compose.resources.painterResource
@@ -54,76 +58,91 @@ fun HomeGraphScreen() {
         }
     }
 
-    Scaffold(
-        containerColor = Surface,
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    AnimatedContent(
-                        targetState = selectedDestination
-                    ) { destination ->
-                        Text(
-                            text = destination.title,
-                            fontFamily = BebasNeueFont(),
-                            fontSize = FontSize.LARGE,
-                            color = TextPrimary
-                        )
-                    }
-                },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(
-                            painter = painterResource(Resources.Icon.Menu),
-                            contentDescription = "Menu icon",
-                            tint = IconPrimary
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Surface,
-                    scrolledContainerColor = Surface,
-                    navigationIconContentColor = IconPrimary,
-                    titleContentColor = TextPrimary,
-                    actionIconContentColor = IconPrimary
-                )
-            )
-        }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(
-                    top = padding.calculateTopPadding(),
-                    bottom = padding.calculateBottomPadding()
-                )
-        ) {
-            //this navHost is for the bottom bar in the home graph screen
-            NavHost(
-                modifier = Modifier.weight(1f),
-                navController = navController,
-                startDestination = Screen.ProductsOverview
-            ) {
-                composable<Screen.ProductsOverview> { }
-                composable<Screen.Cart> { }
-                composable<Screen.Categories> { }
-            }
-            Spacer(modifier = Modifier.height(12.dp))
-            Box(
-                modifier = Modifier.padding(all = 12.dp)
-            ) {
-                BottomBar(
-                    selectedDestination = selectedDestination,
-                    onSelectDestination = { destination ->
-                        navController.navigate(destination.screen) {
-                            launchSingleTop = true
-                            popUpTo<Screen.ProductsOverview> {
-                                saveState = true
-                                inclusive = false
-                            }
-                            restoreState = true
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(SurfaceLighter)
+            .systemBarsPadding()
+    ) {
+        CustomDrawer(
+            onProfileClick = { /*TODO*/ },
+            onBlogClick = { /*TODO*/ },
+            onLocationsClick = { /*TODO*/ },
+            onContactClick = { /*TODO*/ },
+            onSignOutClick = { /*TODO*/ },
+            onAdminClick = { TODO() },
+        )
+        Scaffold(
+            containerColor = Surface,
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        AnimatedContent(
+                            targetState = selectedDestination
+                        ) { destination ->
+                            Text(
+                                text = destination.title,
+                                fontFamily = BebasNeueFont(),
+                                fontSize = FontSize.LARGE,
+                                color = TextPrimary
+                            )
                         }
-                    }
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(
+                                painter = painterResource(Resources.Icon.Menu),
+                                contentDescription = "Menu icon",
+                                tint = IconPrimary
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Surface,
+                        scrolledContainerColor = Surface,
+                        navigationIconContentColor = IconPrimary,
+                        titleContentColor = TextPrimary,
+                        actionIconContentColor = IconPrimary
+                    )
                 )
+            }
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = padding.calculateTopPadding(),
+                        bottom = padding.calculateBottomPadding()
+                    )
+            ) {
+                //this navHost is for the bottom bar in the home graph screen
+                NavHost(
+                    modifier = Modifier.weight(1f),
+                    navController = navController,
+                    startDestination = Screen.ProductsOverview
+                ) {
+                    composable<Screen.ProductsOverview> { }
+                    composable<Screen.Cart> { }
+                    composable<Screen.Categories> { }
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Box(
+                    modifier = Modifier.padding(all = 12.dp)
+                ) {
+                    BottomBar(
+                        selectedDestination = selectedDestination,
+                        onSelectDestination = { destination ->
+                            navController.navigate(destination.screen) {
+                                launchSingleTop = true
+                                popUpTo<Screen.ProductsOverview> {
+                                    saveState = true
+                                    inclusive = false
+                                }
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
             }
         }
     }
---
+++ feature/home/src/commonMain/kotlin/com/nutrisport/home/component/DrawerItemCard.kt
@@ -0,0 +1,51 @@
+package com.nutrisport.home.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.nutrisport.home.domain.DrawerItem
+import com.nutrisport.shared.FontSize
+import com.nutrisport.shared.IconPrimary
+import com.nutrisport.shared.TextPrimary
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun DrawerItemCard(
+    drawerItem: DrawerItem,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(99.dp))
+            .clickable { onClick() }
+            .padding(
+                vertical = 12.dp,
+                horizontal = 12.dp
+            ),
+        verticalAlignment = CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(drawerItem.icon),
+            contentDescription = "Drawer Icon",
+            tint = IconPrimary
+        )
+        Spacer(Modifier.width(12.dp))
+        Text(
+            text = drawerItem.title,
+            color = TextPrimary,
+            fontSize = FontSize.EXTRA_REGULAR
+        )
+    }
+}
